f150d9fe0e6caca57acd8aa6e3fbe9cec0ba6943
RayshineRen <1324789704@qq.com>
2022-05-21 00:51:09 +0800

    hpVPINN for burgers and possion

--- test/hp-VPINN/hp-VPINN_possion.py
+++ test/hp-VPINN/hp-VPINN_possion.py
@@ -28,10 +28,16 @@ if __name__ == '__main__':
     Nx_Quad = 50
     Ny_Quad = 50
     x_quad, w_quad_x, y_quad, w_quad_y = generate_quad_data(Nx_Quad, Ny_Quad)
-    grid_x = np.array([x_l, x_r])
-    grid_y = np.array([y_l, y_h])
-    f_ext = np.array((Nx_testfcn * Ny_testfcn) * [0])[:, None]
-    f_ext_total = np.tile(f_ext, (1, 1)).reshape(-1, Nx_testfcn * Ny_testfcn, 1)
+    # 区域划分
+    delta_x = (x_r - x_l) / NEx
+    # 区域网格点 eg. 均分为两个区域[-1, 0, 1]
+    grid_x = np.asarray([x_l + i * delta_x for i in range(NEx + 1)])
+    # 每个区域内测试函数的数目，目前都一样，结果只取决于区域内的求积点
+    delta_y = (y_h - y_l) / NEy
+    grid_y = np.asarray([y_l + i * delta_y for i in range(NEy + 1)])
+    # 计算Fk NEx * NEy 个区域
+    F_ext = np.array((Nx_testfcn * Ny_testfcn) * [0])[:, None]
+    F_ext_total = np.tile(F_ext, (NEx * NEy, 1)).reshape(-1, Nx_testfcn * Ny_testfcn, 1)
     # possion Data
     data = PossionData(x_l, x_r, y_l, y_h)
     data.generate_ibc(n_u)
@@ -39,5 +45,5 @@ if __name__ == '__main__':
     # PINN for possion equation
     model = VPINN_possion(data.x_u_train, data.u_train, data.x_f, layers,
                           maxIter, activation, lr, opt, extended,
-                          x_quad, w_quad_x, y_quad, w_quad_y, f_ext_total, grid_x, grid_y)
+                          x_quad, w_quad_x, y_quad, w_quad_y, F_ext_total, grid_x, grid_y)
     data.run_model(model)
